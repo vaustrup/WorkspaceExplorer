89a3def241f88fffea410ccebd1b2df29aee55e0
Volker Austrup <volker.andreas.austrup@cern.ch>
2022-09-21 09:19:18 +0200

    add link to github

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,12 @@ import AnalysesList from './components/AnalysesList.vue'
   <header>
     <div>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      	<a class="navbar-brand" href="#" style="padding-left:1em">WorkSpaceX</a>
+	      <div class="container-fluid">
+	      	<a class="navbar-brand" href="#" style="padding-left:1em">WorkSpaceX</a>
+	      	<a class="d-flex" href="https://github.com/vaustrup/WorkspaceExplorer">
+	      		<img src="/public/GitHub-Mark-Light-64px.png" height="30px" />
+	      	</a>
+	      </div>
       </nav>
     </div>
   </header>
